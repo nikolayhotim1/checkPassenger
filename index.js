7fd1b6ac6c284f254f16f86bb8894c1ad696beed
nikolayhotim1 <hotim_nikolay@mail.ru>
2021-09-22 23:18:58 +0300

    refactoring: add print passenger function

--- index.js
+++ index.js
@@ -1,4 +1,19 @@
 'usestrict';
+let passengers = [{ name: "Jane Doloop", paid: true },
+{ name: "Dr. Evel", paid: true },
+{ name: "Sue Property", paid: false },
+{ name: "John Funcall", paid: true }];
+
+function processPassengers(passengers, testFunction) {
+    for (let i = 0; i < passengers.length; i++) {
+        if (testFunction(passengers[i])) {
+            return false;
+        }
+    }
+
+    return true;
+}
+
 function checkNoFlyList(passenger) {
     return (passenger.name === 'Dr. Evel');
 }
@@ -7,10 +22,19 @@ function checkNotPaid(passenger) {
     return (!passenger.paid);
 }
 
-let passenger = {
-    name: "Dr. Evel",
-    paid: false
-};
+function printPassenger(passenger) {
+    console.log(passenger.name + ', paid: ' + passenger.paid);
+    return false;
+}
 
-console.log(checkNoFlyList(passenger)); // true
-console.log(checkNotPaid(passenger)); // true
+let allCanFly = processPassengers(passengers, checkNoFlyList);
+if (!allCanFly) {
+    console.log(`The plane can't take off: we have a passenger on the no-fly-list.`);
+}
+
+let allPaid = processPassengers(passengers, checkNotPaid);
+if (!allPaid) {
+    console.log(`The plane can't take off: not everyone has paid.`);
+}
+
+processPassengers(passengers, printPassenger);
